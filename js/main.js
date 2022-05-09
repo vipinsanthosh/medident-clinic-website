5570549fc25984ac66f39d38d8718a128c9d6671
Vipin Santhosh <vipinsanthosh96@gmail.com>
2022-05-09 19:06:34 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,150 @@
+(function ($) {
+  $(".navbar-brand-1").addClass("logo-color");
+  ("use strict");
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
+    } else {
+      $(".navbar").removeClass("sticky-top shadow-sm");
+    }
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < 45) {
+      $(".navbar-brand-1").addClass("logo-color");
+    } else {
+      $(".navbar-brand-1").removeClass("logo-color");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  // Client carousel
+  $(".client-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 90,
+    dots: false,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 3,
+      },
+      768: {
+        items: 4,
+      },
+      992: {
+        items: 5,
+      },
+      1200: {
+        items: 6,
+      },
+    },
+  });
+  $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on("click", function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $("a").each(function () {
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+
+      var target = this.hash,
+        menu = target;
+      $target = $(target);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $target.offset().top + 2,
+          },
+          500,
+          "swing",
+          function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+          }
+        );
+    });
+  });
+
+  function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $("#menu-center a").each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (
+        refElement.position().top <= scrollPos &&
+        refElement.position().top + refElement.height() > scrollPos
+      ) {
+        $("#menu-center ul li a").removeClass("active");
+        currLink.addClass("active");
+      } else {
+        currLink.removeClass("active");
+      }
+    });
+  }
+})(jQuery);
